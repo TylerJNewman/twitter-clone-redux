2ad0a36836e454563cb31a3216f8e8509df48796
Tyler Newman <tylerjnewman18@gmail.com>
2020-06-24 17:00:05 -0700

    Add loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,14 +2,17 @@ import { getInitialData } from "../utils/api";
 import { receiveUsers } from "../actions/users";
 import { receiveTweets } from "../actions/tweets";
 import { setAuthedUser } from "../actions/authedUser";
+import { showLoading, hideLoading } from "react-redux-loading";
 
 const AUTHED_ID = "tylermcginnis";
 
 export const handleInitialData = () => {
   return async (dispatch) => {
+    dispatch(showLoading());
     const { users, tweets } = await getInitialData();
     dispatch(receiveUsers(users));
     dispatch(receiveTweets(tweets));
     dispatch(setAuthedUser(AUTHED_ID));
+    dispatch(hideLoading());
   };
 };
