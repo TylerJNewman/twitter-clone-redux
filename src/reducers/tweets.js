3d3c5c0f9403ba4fc9481549a543a66dc38f811e
Tyler Newman <tylerjnewman18@gmail.com>
2020-06-24 19:43:41 -0700

    Add liking tweet using optimistic UI update

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,12 +1,26 @@
-import { RECEIVE_TWEETS } from "../actions/tweets";
+import { RECEIVE_TWEETS, TOGGLE_TWEET } from "../actions/tweets";
 
-const tweets = (state = {}, action) => {
+export default function tweets(state = {}, action) {
   switch (action.type) {
     case RECEIVE_TWEETS:
-      return { ...state, ...action.tweets };
+      return {
+        ...state,
+        ...action.tweets,
+      };
+    case TOGGLE_TWEET:
+      return {
+        ...state,
+        [action.id]: {
+          ...state[action.id],
+          likes:
+            action.hasLiked === true
+              ? state[action.id].likes.filter(
+                  (uid) => uid !== action.authedUser
+                )
+              : state[action.id].likes.concat([action.authedUser]),
+        },
+      };
     default:
       return state;
   }
-};
-
-export default tweets;
+}
